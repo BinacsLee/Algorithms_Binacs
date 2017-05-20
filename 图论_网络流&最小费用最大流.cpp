75eb6ab047954392e481be7e8195e033d08e4dfc
BinacsLee <bin646891055@gmail.com>
2017-05-20 12:08:10 +0800

    Update 图论_网络流&最小费用最大流.cpp

--- 图论_网络流&最小费用最大流.cpp
+++ 图论_网络流&最小费用最大流.cpp
@@ -1,3 +1,4 @@
+/*----------------网络流-----------------*/
 const int maxn=1005;
 const int INF=0x3f3f3f3f;
 struct Edge{
@@ -72,3 +73,72 @@ struct Dinic{
         return flow;
     }
 };
+
+
+/*-----------------最小费用最大流------------------*/
+const int maxn=505;
+const int INF=0x3f3f3f3f;
+
+struct Edge{
+    int from,to,cap,flow,cost;
+};
+
+struct MCMF{
+    int n,m,s,t;
+    vector<Edge> edges;
+    vector<int> G[maxn];
+    int inq[maxn];  // 是否在队列中
+    int d[maxn];    // bellman-ford
+    int p[maxn];    // 上一条弧
+    int a[maxn];    // 可改进量
+    
+    void init(int n){
+        this->n=n;
+        for(int i=0;i<=n;i++) G[i].clear();
+        edges.clear();
+    }
+    void addedge(int from,int to,int cap,int cost){
+        edges.push_back((Edge){from,to,cap,0,cost});
+        edges.push_back((Edge){to,from,0,0,-cost});
+        m=edges.size();
+        G[from].push_back(m-2);
+        G[to].push_back(m-1);
+    }
+    bool BellmanFord(int s,int t,int &flow,int &cost){
+        for(int i=0;i<=n;i++) d[i]=INF;
+        memset(inq,0,sizeof(inq));
+        d[s]=0;inq[s]=1;p[s]=0;a[s]=INF;
+        
+        queue<int> Q;
+        Q.push(s);
+        while(!Q.empty()){
+            int u=Q.front();Q.pop();
+            inq[u]=0;
+            for(int i=0;i<G[u].size();i++){
+                Edge& e=edges[G[u][i]];
+                if(e.cap>e.flow&&d[e.to]>d[u]+e.cost){
+                    d[e.to]=d[u]+e.cost;
+                    p[e.to]=G[u][i];
+                    a[e.to]=min(a[u],e.cap-e.flow);
+                    if(!inq[e.to]){Q.push(e.to);inq[e.to]=1;}
+                }
+            }
+        }
+        if(d[t]==INF) return false;
+        flow+=a[t];
+        cost+=d[t]*a[t];
+        int u=t;
+        while(u!=s){
+            edges[p[u]].flow+=a[t];
+            edges[p[u]^1].flow-=a[t];
+            u=edges[p[u]].from;
+        }
+        return true;
+    }
+    int Mincost(int s,int t){
+        int flow=0,cost=0;
+        while(BellmanFord(s,t,flow,cost));
+        return cost;
+    }
+};
+
