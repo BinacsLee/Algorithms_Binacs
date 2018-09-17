bb50d63149c08f40144b8a78e2917e8ea2ce8c50
Binacs <bin646891055@gmail.com>
2018-09-17 21:09:02 +0800

    Update 其它_快速读入.cpp

--- 其它_快速读入.cpp
+++ 其它_快速读入.cpp
@@ -5,3 +5,10 @@ void read(int &x){
     for (; ch >= '0' && ch <= '9'; ch = getchar())
         x = x * 10 + ch - '0';
 }
+
+
+//读一行
+ while(scanf("%lld",&a[n1++])){
+     char ch=getchar();
+     if(ch=='\n') break;
+ }
